f46959a3dc1d15e7d98e1580e87f76537c2706b7
Adam Cusanno <cusannoadam@gmail.com>
2024-08-18 14:25:35 +0200

    Added level1

--- Assets/Scripts/TextFade.cs
+++ Assets/Scripts/TextFade.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class TextFade : MonoBehaviour
 {
 	[SerializeField]
 	TextMeshPro text;
+
+	public void Awake()
+	{
+		text.alpha = 0f;
+	}
 
 	public void FadeIn()
 	{
