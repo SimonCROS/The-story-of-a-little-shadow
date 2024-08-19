5586d3a39a2f1b56dd484b1d929f72d7856c7f6c
Simon Cros <simon.dc.cros@gmail.com>
2024-08-19 22:27:59 +0200

    Spider mode and jump !

--- Assets/Scripts/Spider.cs
+++ Assets/Scripts/Spider.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Spider : MonoBehaviour
@@ -5,7 +6,12 @@ public class Spider : MonoBehaviour
 	private CharacterController controller;
 	private SpriteRenderer spriteRenderer;
 	private Vector3 velocity;
+	private float lastJumpTime = float.MinValue;
 	public float gravityValue = -9.81f;
+	public float jumpHeight = 2f;
+	public float jumpCooldown = 5f;
+	public float speadIncrementer = 8f;
+	public float moveSpeed = 5f;
     public int damage;
     public PlayerController playerController;
     public Sprite groundedSprite;
@@ -18,7 +24,12 @@ public class Spider : MonoBehaviour
 	    controller = GetComponent<CharacterController>();
 	    spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
-    
+
+    private void OnEnable()
+    {
+	    lastJumpTime = Time.fixedTime;
+    }
+
     private void Update()
     {
 	    Sprite sprite = controller.isGrounded ? groundedSprite : jumpSprite;
@@ -34,6 +45,19 @@ public class Spider : MonoBehaviour
 	    if (IsGrounded && velocity.y < 0)
 	    {
 		    velocity.y = 0f;
+	    }
+
+	    if (IsGrounded)
+	    {
+		    Vector3 directionToPlayer = (playerController.transform.position - transform.position).normalized;
+		    velocity.x += directionToPlayer.x * speadIncrementer * Time.deltaTime;
+		    velocity.x = Mathf.Clamp(velocity.x, -moveSpeed, moveSpeed);
+	    
+		    if (Time.fixedTime - lastJumpTime > jumpCooldown)
+		    {
+			    lastJumpTime = Time.fixedTime;
+			    velocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+		    }
 	    }
 	    
 	    velocity.y += gravityValue * Time.deltaTime;
